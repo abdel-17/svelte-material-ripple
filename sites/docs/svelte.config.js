0c5c4e134b0a1605eb47a0bf1baad88099698402
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-09-27 20:00:51 +0300

    use adapter-static

--- sites/docs/svelte.config.js
+++ sites/docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsx } from "mdsx";
 import mdsxConfig from "./mdsx.config.js";
